780d393062e94ce8edd532df173d7c18d3e88340
ElijahDavila <elijahdavila96@gmail.com>
2020-05-12 23:09:49 -0400

    Sorting Assignment

---
+++ SortingAssignment.java
@@ -0,0 +1,25 @@
+public class SortingAssignment {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        for(int i = 0; i <  array.length; i++) {
+            array[i] = (int)(Math.random() * 100);
+            System.out.print(array[i]);
+        }
+    }
+    public static void bubbleSort(int[] array) {
+        boolean sorted = false;
+        int temp;
+        while(!sorted) {
+            sorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i+1]) {
+                    temp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+    }
+}
+
